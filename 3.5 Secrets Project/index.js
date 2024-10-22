da9591a4208bae5027c8996db94f727c3e03e1d7
govindbhoot <84218766+govindbhoot@users.noreply.github.com>
2024-10-22 16:33:17 +0530

    Add files via upload

---
+++ 3.5 Secrets Project/index.js
@@ -0,0 +1,44 @@
+
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app= express();
+const port=3000;
+var userIsAuthorised=false;
+
+app.use(express.urlencoded({extended:true}))
+
+function passwordCheck(req,res,next){
+    
+    const password=req.body["password"];
+    if(password==="ILoveProgramming"){
+        userIsAuthorised=true;
+    }
+    
+    next()
+  }
+  app.use(passwordCheck);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname  +"/public/index.html")
+})
+
+app.post("/check",(req,res)=>{
+   if(userIsAuthorised){
+    res.sendFile(__dirname +"/public/secret.html")
+   }
+    else{
+        res.sendFile(__dirname+ "/public/index.html")
+    }
+
+})
+
+
+app.listen(port,()=>{
+    console.log(`listening on port: ${port}`)
+})
+
